1c53f6c9482a4e4283e88f3509ca80f51b147155
neamul haque khan <neamul.naem@hotmail.com>
2020-04-13 01:18:30 +0600

    Adding IUnitOfWork Interface

---
+++ DAL/UnitOfWork/IUnitOfWork.cs
@@ -0,0 +1,17 @@
+﻿using DAL.IRepositories.CryptoLearn;
+using System;
+
+namespace DAL.UnitOfWork
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        IDeviceRepository Devices { get; }
+        ILanguageRepository Languages { get; }
+        ILessonRepository Lessons { get; }
+        ILessonDescriptionRepository LessonDescriptions { get; }
+        ILessonQuestionAndOptionRepository LessonQuestionsDetails { get; }
+        ISectionRepository Sections { get; }
+
+        int Complete();
+    }
+}
