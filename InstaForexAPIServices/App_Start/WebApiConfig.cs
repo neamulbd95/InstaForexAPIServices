d6411a190cf5a1a7ce22be81d10ed6e2161aaf9c
neamul haque khan <neamul.naem@hotmail.com>
2020-04-13 01:38:31 +0600

    JSON configuration

--- InstaForexAPIServices/App_Start/WebApiConfig.cs
+++ InstaForexAPIServices/App_Start/WebApiConfig.cs
@@ -16,6 +16,10 @@ namespace InstaForexAPIServices
                 routeTemplate: "api/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
+
+            var json = config.Formatters.JsonFormatter;
+            json.SerializerSettings.PreserveReferencesHandling = Newtonsoft.Json.PreserveReferencesHandling.Objects;
+            config.Formatters.Remove(config.Formatters.XmlFormatter);
         }
     }
 }
