f5a8e00465851044c8581a4dddf84e5e210340e3
neamul haque khan <neamul.naem@hotmail.com>
2020-04-20 15:43:16 +0600

    Add Device Controller

---
+++ InstaForexAPIServices/Controllers/DeviceController.cs
@@ -0,0 +1,36 @@
+﻿using DAL.Domain.CryptoLearn;
+using DAL.UnitOfWork;
+using InstaForexAPIServices.Response;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Unity;
+
+namespace InstaForexAPIServices.Controllers
+{
+    public class DeviceController : ApiController
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        IUnityContainer container = new UnityContainer();
+
+        public DeviceController(IUnitOfWork unitOfWork)
+        {
+            this._unitOfWork = unitOfWork;
+        }
+
+        [HttpGet]
+        [Route("api/CryptoLearn/v1/GetDevices")]
+        public GeneralResponse<Device> GetDevice()
+        {
+            var result = container.Resolve<GeneralResponse<Device>>();
+
+            result.ResponseCode = HttpStatusCode.OK;
+            result.ResponseMessage = "Request is okay";
+            result.Result = _unitOfWork.Devices.GetAll();
+            return result;
+        }
+    }
+}
