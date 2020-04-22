59a89c53a24359c66e74f46c084530c8aaf1b2a0
neamul haque khan <neamul.naem@hotmail.com>
2020-04-22 22:06:26 +0600

    adding 'checkName' method to UserInfo controller

---
+++ InstaForexAPIServices/Controllers/IFXGame/UserInfoController.cs
@@ -0,0 +1,66 @@
+﻿using DAL.UnitOfWork;
+using InstaForexAPIServices.Response;
+using InstaForexAPIServices.Response.IFXGame;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Unity;
+
+namespace InstaForexAPIServices.Controllers.IFXGame
+{
+    public class UserInfoController : ApiController
+    {
+        private readonly IUnitOfWorkIFXGame _unitOfWork;
+        IUnityContainer container = new UnityContainer();
+
+        public UserInfoController(IUnitOfWorkIFXGame unitOfWork)
+        {
+            this._unitOfWork = unitOfWork;
+        }
+
+        [HttpGet]
+        [Route("api/IFXGame/v1/CheckNickName")]
+        public  GeneralResponse<ErroMessage> CheckName(string nickName)
+        {
+            var result = container.Resolve<GeneralResponse<ErroMessage>>();
+            var message = container.Resolve<ErroMessage>();
+
+            if (string.IsNullOrEmpty(nickName))
+            {
+                result.ResponseCode = HttpStatusCode.BadRequest;
+                result.ResponseMessage = "Request is wrong";
+                result.Result = null;
+                return result;
+            }
+            else
+            {
+                var user = _unitOfWork.UserInfo.GetSingle(x => x.NickName.ToUpper() == nickName.ToUpper());
+
+                if(user == null)
+                {
+                    message.ErrorStatus = false;
+                    message.Message = "This nick name is available";
+
+                    result.ResponseCode = HttpStatusCode.OK;
+                    result.ResponseMessage = "Request is okay";
+                    result.Result = (IEnumerable<ErroMessage>)message;
+                    return result;
+                }
+
+                else
+                {
+                    message.ErrorStatus = true;
+                    message.Message = "Such nick name has been already existed";
+
+                    result.ResponseCode = HttpStatusCode.OK;
+                    result.ResponseMessage = "Request is okay";
+                    result.Result = (IEnumerable<ErroMessage>)message;
+                    return result;
+                }
+            }
+        }
+    }
+}
---
+++ InstaForexAPIServices/Response/IFXGame/ErroMessage.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace InstaForexAPIServices.Response.IFXGame
+{
+    public class ErroMessage
+    {
+        public bool ErrorStatus { get; set; }
+        public string Message { get; set; }
+    }
+}
